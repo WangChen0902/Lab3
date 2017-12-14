234135620a5a6aa52d724b7029430037ec5ba580
WangChen0902 <827913668@qq.com>
2017-12-14 16:26:48 +0800

    最终修改

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -7,6 +7,23 @@
 using namespace std;
 
 const string SRC = "D:\\WCs\\Lab3\\Lab3\\input.txt";
+const string SAVE = "D:\\WCs\\Lab3\\Lab3\\output.txt";
+
+//二分搜索算法，默认dic是按字典序排列的有序数组
+int bSearch(string array[], int low, int high, string target)
+{
+	while (low <= high)
+	{
+		int mid = (low + high) / 2;
+		if (array[mid]>target)
+			high = mid - 1;
+		else if (array[mid]<target)
+			low = mid + 1;
+		else
+			return mid;
+	}
+	return -1;
+}
 
 int main()
 {
@@ -16,7 +33,6 @@ int main()
 	inf >> n;
 	string dic[128];
 	string text[128];
-	//cout << n << endl;
 	for (int i = 0; i < n; i++) {
 		inf >> dic[i];
 		//cout << dic[i] << endl;
@@ -28,6 +44,28 @@ int main()
 	}
 	inf.close();
 
+	map<string, int> textMap;
+	for (int i = 0; i < m; i++) {
+		textMap.insert(pair<string,int>(text[i],0));
+	}
+	//对dic二分搜索text中的单词
+	map<string, int>::iterator it = textMap.begin();
+	for (int i = 0; i < m;i++) {
+		int flag = bSearch(dic, 0, n - 1, text[i]);
+		if (flag == -1) {
+			textMap.find(text[i])->second++;
+		}
+	}
+	
+	int max = 0;
+	for (it; it != textMap.end(); it++) {
+		if (it->second > max) {
+			max = it->second;
+		}
+	}
+	//cout << max << endl;
+	ofstream save(SAVE);
+	save << max;
     return 0;
 }
 
